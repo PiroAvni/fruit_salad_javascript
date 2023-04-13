5566ffadf14cfa5d58cbd27762c642db9ad41d6d
“PiroAvni” <avnipiro.dev@gmail.com>
2023-04-13 08:38:09 +0100

    Fruit salad app v2

--- static/js/fruit.js
+++ static/js/fruit.js
@@ -1,8 +1,6 @@
 const fruitList = document.querySelector("#fruitSection ul");
 const fruitNutrition = document.querySelector("#nutritionSection p");
 
-console.log(nutritionSection);
-
 function extractFruit(event) {
   event.preventDefault();
   console.log(event.target.fruitInput.value);
@@ -17,58 +15,34 @@ function extractFruit(event) {
   event.target.fruitInput.value = "";
 }
 
- let cal = 0;
+let totalCalories = 0;
 function addFruit(fruit) {
   const li = document.createElement("li");
   li.textContent = `${fruit.name} ${fruit.nutritions.calories} calories`;
-
   li.addEventListener("click", removeFruit, { once: true }); // once use, event listener will be removed
   fruitList.appendChild(li);
- 
-  cal += fruit.nutritions.calories;
+  displayTotalCalories(fruit)
 
-  if (cal) {
-    fruitNutrition.classList.add("total");
-  }
-    fruitNutrition.textContent = `${cal} calories`;
   function removeFruit(e) {
     const choice = confirm("Are you sure?");
     choice ? e.target.remove() : "";
- 
     
-    cal -= fruit.nutritions.calories;
-    fruitNutrition.textContent = `${cal} calories`;
+    totalCalories -= fruit.nutritions.calories;
+    fruitNutrition.textContent = `${totalCalories} calories`;
    
   }
 }
 
-// let totalCalories = 0;
-
-// function displayTotalCalories({nutritions}){
-//   console.log(nutritions.calories)
+function displayTotalCalories({nutritions}){
+  console.log(nutritions.calories)
   
-//     totalCalories += nutritions.calories;
-//  console.log('displayTotal Func;',totalCalories)
-//   if (totalCalories) {
-//     fruitNutrition.classList.add("total");
-//     fruitNutrition.textContent = `${totalCalories} calories`;
-//   }
-// }
-
-
-// function removeFruit(e,fruit) {
-//   const choice = confirm("Are you sure?");
-//   choice ? e.target.remove() : "";
-//   totalCalories -= fruit.nutritions.calories;
-//   console.log('total:',totalCalories)
-//   //displayTotalCalories()
-   
-//   fruitNutrition.textContent = `${totalCalories} calories`
-// }
-
-
-
-
+    totalCalories += nutritions.calories;
+ console.log('displayTotal Func;',totalCalories)
+  if (totalCalories) {
+    fruitNutrition.classList.add("total");
+    fruitNutrition.textContent = `${totalCalories} calories`;
+  }
+}
 
 // Fetch API
 function fetchFruitData(fruit) {
